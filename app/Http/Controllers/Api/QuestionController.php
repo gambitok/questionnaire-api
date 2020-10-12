aae1580a2c57f36cc3b832236d53872e2a1a6414
gambitok <gambitokgd@gmail.com>
2020-10-12 23:33:15 +0300

    create secret token for `questions`

--- app/Http/Controllers/Api/QuestionController.php
+++ app/Http/Controllers/Api/QuestionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Question;
+use Illuminate\Support\Str;
 
 /**
  * Class QuestionController
@@ -12,5 +13,91 @@ use App\Models\Question;
  */
 class QuestionController extends Controller
 {
-    //
+    /**
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $questions = Question::get()->toJson(JSON_PRETTY_PRINT);
+        return response($questions, 200);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\JsonResponse|\Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        if (Question::where('secret', $id)->exists()) {
+            $question = Question::where('secret', $id)->get()->toJson(JSON_PRETTY_PRINT);
+            return response($question, 200);
+        } else {
+            return response()->json([
+                "message" => "Question not found"
+            ], 404);
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $question = new Question;
+        $question->name = is_null($request->name) ? 'empty' : $request->name;
+        do {
+            $secret = Str::random(1);
+        } while (Question::where("secret", "=", $secret)->first());
+        $question->secret = $secret;
+
+        $question->save();
+
+        return response()->json([
+            "message" => "Question record created"
+        ], 201);
+    }
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id)
+    {
+        if (Question::where('id', $id)->exists()) {
+            $question = Question::find($id);
+
+            $question->name = is_null($request->name) ? $question->name : $request->name;
+            $question->save();
+
+            return response()->json([
+                "message" => "Records updated successfully"
+            ], 200);
+        } else {
+            return response()->json([
+                "message" => "Question not found"
+            ], 404);
+        }
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        if(Question::where('id', $id)->exists()) {
+            $question = Question::find($id);
+            $question->delete();
+
+            return response()->json([
+                "message" => "Records deleted"
+            ], 202);
+        } else {
+            return response()->json([
+                "message" => "Question not found"
+            ], 404);
+        }
+    }
 }
