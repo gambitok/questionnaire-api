d9a8eeb6628078be919decf62157a1c6dcb2f63f
gambitok <gambitokgd@gmail.com>
2020-10-19 10:59:44 +0300

    init local docker server (php+nginx+mysql)

--- docker-compose/mysql/init_db.sql
+++ docker-compose/mysql/init_db.sql
@@ -1,9 +1,3 @@
-DROP TABLE IF EXISTS `questionnaire`;
+DROP TABLE IF EXISTS `questions`;
 
-CREATE TABLE `questionnaire` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-INSERT INTO `questionnaire` (name) VALUES ('question 1'), ('question 2'), ('question 3');
+DROP TABLE IF EXISTS `answers`;
